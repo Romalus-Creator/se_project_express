3c95f39e1738c358d7d3e1a8d63b95030bda41da
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-10-01 20:53:24 -0500

    Feat: Add express-rate-limit and Helmet middlewares per reviewer's recommendation.

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const { errors } = require("celebrate");
 const mainIndex = require("./routes/index");
 const errorHandler = require("./middlewares/errorhandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { rateLimit } = require("express-rate-limit");
+const helmet = require("helmet");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 app.use(cors());
 
@@ -21,6 +32,8 @@ mongoose
 
 app.use(requestLogger);
 app.use(express.json());
+app.use(limiter);
+app.use(helmet());
 
 app.get("/crash-test", () => {
   setTimeout(() => {
